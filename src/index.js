eb7f7cb6156fed8d2301ba9afdd721a5ca1d1b76
Maksim Semenov <maksimuma@list.ru>
2023-03-23 20:08:28 +0300

    feat: добавить роуты регистрации и авторизации, защитить роут '/'

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './pages/index.css';
 import App from './components/App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Router>
+        <App />
+    </Router>
 );
